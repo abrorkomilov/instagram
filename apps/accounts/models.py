024ca6365cacb40a5724632bd8ecd734fa43d16e
abrorjon <abrorkomilov571@gmail.com>
2024-11-15 10:00:07 +0500

    modellar yaratildi

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,12 +2,22 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from apps.base.models import BaseModel
 
 
-class User(AbstractUser):
+class User(AbstractUser, BaseModel):
     avatar = models.ImageField(upload_to="avatars/")
 
     def __str__(self) -> str:
         return self.username
     
-#class data():
+class Chat(BaseModel):
+    users = models.ManyToManyField(User, related_name='user_chats')
+
+class Message(BaseModel):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE
+                             related_name='chat_messages')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL
+                               null=True, related_name='user_messages')
+    text = models.TextField()
+    
--- apps/structure/models.py
+++ apps/structure/models.py
@@ -16,3 +16,43 @@ class Photo(BaseModel):
     desc = models.CharField(max_length=250)
     tags = models.ManyToManyField(Tag, related_name="tag_photos")
 
+    def __str__(self) -> str:
+        return self.desc + "/t|\t" + self.author.username
+    
+    class BookMark(BaseModel):
+        author = models.ForeignKey(User, on_delete=models.CASCADE
+                                   related_name="user_bookmarks")
+        target = models.ForeignKey(Photo, on_delete=models.CASCADE, 
+                                   related_name="photo_bookmarks")
+        
+    def __str__(self) -> str:
+        return f"{self.author.username} -> {self.target.desc}"
+
+class Like(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE
+                                   related_name="user_likes")
+    target = models.ForeignKey(Photo, on_delete=models.CASCADE, 
+                                   related_name="photo_likes")
+    
+    def __str__(self) -> str:
+        return f"{self.author.username} -> {self.target.desc}"
+    
+class Commet(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE
+                                   related_name="user_comments")
+    target = models.ForeignKey(Photo, on_delete=models.CASCADE, 
+                                   related_name="photo_comments")
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.author.username} -> {self.target.desc}"
+    
+class Subscribe(BaseModel):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, 
+                              related_name='user_subscribes')
+    target = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='user_subscibers')
+    
+    def __str__(self) -> str:
+        return f"{self.autor.username} -> {self.target.username}"
+    
